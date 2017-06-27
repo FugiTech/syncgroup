29550e250665d92c84e5d598a57fd41c32f23164
P. Michael Ossareh <ossareh@gmail.com>
2017-06-27 22:30:18 +0200

    remove use of sync.WaitGroup

--- syncgroup.go
+++ syncgroup.go
@@ -1,39 +1,47 @@
 package syncgroup
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
-// SyncGroup is a sync.WaitGroup that also allows tracked goroutines to "sync" their execution.
+// SyncGroup acts as a sync.WaitGroup which allows goroutes to Sync on one another.
 // The use case it was designed for batching, where you wish to accumulate something until it
 // reaches a certain size or time (tracked via the batcher), or if work is blocked on the batching.
 type SyncGroup struct {
-	running  sync.WaitGroup
-	syncing  sync.WaitGroup
-	finished sync.RWMutex
+	queued  int32
+	stopped int32
+	paused  int32
 
-	lock  sync.Mutex
-	count int
+	finished *sync.Cond
+	once     sync.Once
+}
+
+// Add sets how many jobs are being queued up
+func (sg *SyncGroup) Add(j int) {
+	sg.once.Do(func() {
+		sg.finished = sync.NewCond(&sync.Mutex{})
+	})
+
+	sg.finished.L.Lock()
+	atomic.AddInt32(&sg.queued, int32(j))
+	sg.finished.L.Unlock()
 }
 
 // Go runs a function in a goroutine, tracking it's execution state in the SyncGroup
 // The supplied function can call (*SyncGroup).Sync() to pause execution until all
 // tracked goroutines are also in a call to Sync()
 func (sg *SyncGroup) Go(fn func()) {
-	sg.lock.Lock()
-	defer sg.lock.Unlock()
-
-	sg.running.Add(1) // Must be called before sg.watch or it will immediately terminate
-	if sg.count == 0 {
-		sg.finished.Lock()
-		go sg.watch()
+	if sg.finished.L == nil {
+		panic("Cannot call SyncGroup.Go before SyncGroup.Add")
 	}
-	sg.count++
 
 	go func() {
 		defer func() {
-			sg.lock.Lock()
-			defer sg.lock.Unlock()
-			sg.count-- // Must be called before calling sg.running.Done() or sg.watch() will break
-			sg.running.Done()
+			sg.finished.L.Lock()
+			atomic.AddInt32(&sg.stopped, 1)
+			sg.finished.L.Unlock()
+			sg.finished.Broadcast()
 		}()
 
 		fn()
@@ -42,35 +50,39 @@ func (sg *SyncGroup) Go(fn func()) {
 
 // Wait will block until all tracked goroutines finish executing
 func (sg *SyncGroup) Wait() {
-	sg.finished.RLock()
-	sg.finished.RUnlock()
-}
-
-// Sync will block execution until all other tracked goroutines call Sync() or terminate
-func (sg *SyncGroup) Sync() {
-	sg.syncing.Add(1)
-	sg.running.Done()
-	sg.syncing.Wait()
-}
-
-// Watch maintains the (*SyncGroup).finished lock (used in Wait) and
-// calls Done on the syncing WaitGroup when deadlock is reached
-func (sg *SyncGroup) watch() {
-	defer sg.finished.Unlock()
-
 	for {
-		sg.running.Wait() // Wait for all goroutines to Sync() or terminate
-		// Theoretically there's a race condition here where you could call Go() between Wait() and Lock() running...
-		// But I don't know how to get around that
-		// If it _does_ happen, the calls to Add() below will have the wrong count and will likely panic
-		sg.lock.Lock()
-		if sg.count == 0 { // All done!
-			sg.lock.Unlock()
+		sg.finished.L.Lock()
+		if atomic.LoadInt32(&sg.queued) == atomic.LoadInt32(&sg.stopped) {
+			sg.finished.L.Unlock()
 			return
 		}
-
-		sg.running.Add(sg.count)
-		sg.syncing.Add(-1 * sg.count)
-		sg.lock.Unlock()
+		sg.finished.Wait()
+		sg.finished.L.Unlock()
 	}
+}
+
+// Sync will block until all goroutines are either finished or waiting on Sync
+func (sg *SyncGroup) Sync() {
+	sg.finished.L.Lock()
+	atomic.AddInt32(&sg.paused, 1)
+	sg.finished.L.Unlock()
+	sg.finished.Broadcast()
+
+	for {
+		sg.finished.L.Lock()
+		if sg.done() {
+			atomic.AddInt32(&sg.paused, -1)
+			sg.finished.L.Unlock()
+			return
+		}
+		sg.finished.Wait()
+		sg.finished.L.Unlock()
+	}
+}
+
+func (sg *SyncGroup) done() bool {
+	if atomic.LoadInt32(&sg.queued) == (atomic.LoadInt32(&sg.stopped) + atomic.LoadInt32(&sg.paused)) {
+		return true
+	}
+	return false
 }
---
+++ syncgroup_test.go
@@ -0,0 +1,129 @@
+package syncgroup_test
+
+import (
+	"sync/atomic"
+	"testing"
+
+	"github.com/ossareh/syncgroup"
+)
+
+func TestSyncGroup_Wait(t *testing.T) {
+	// In the most basic case SyncGroup should behave like a (*sync.WaitGroup)
+	iters := 2000000
+	decrements := int32(iters)
+
+	sg := &syncgroup.SyncGroup{}
+	sg.Add(iters)
+
+	for i := 0; i < iters; i++ {
+		sg.Go(func() {
+			atomic.AddInt32(&decrements, -1)
+		})
+	}
+	sg.Wait()
+
+	if atomic.LoadInt32(&decrements) != 0 {
+		t.Errorf("TestSyncGroup_Wait expected=0 got=%d", decrements)
+	}
+}
+
+func TestSyncGroup_Sync(t *testing.T) {
+	// The real purpose of SyncGroup is to allow functions to Sync() on eachother
+	iters := 200000
+	decrements := int32(iters)
+	increments := int32(0)
+
+	sg := &syncgroup.SyncGroup{}
+	sg.Add(iters)
+
+	for i := 0; i < iters; i++ {
+		sg.Go(func() {
+			atomic.AddInt32(&decrements, -1)
+			sg.Sync()
+
+			if atomic.LoadInt32(&decrements) != 0 {
+				t.Errorf("TestSyncGroup_Sync goroutine expected=0 got=%d", decrements)
+			}
+			atomic.AddInt32(&increments, 2)
+		})
+	}
+	sg.Wait()
+
+	if atomic.LoadInt32(&increments) != int32(iters*2) {
+		t.Errorf("TestSyncGroup_Sync expected=%d got=%d", iters*2, increments)
+	}
+}
+
+func TestSyncGroup_Mixed(t *testing.T) {
+	// Covers the case of mixed function types, some that sync and some that don't
+	iters := 2000000
+	decrements := int32(iters)
+	increments := int32(0)
+
+	sg := &syncgroup.SyncGroup{}
+	sg.Add(iters)
+
+	for i := 0; i < iters; i++ {
+		if i%10 == 0 {
+			sg.Go(func() {
+				atomic.AddInt32(&decrements, -1)
+				sg.Sync()
+
+				if atomic.LoadInt32(&decrements) != 0 {
+					t.Errorf("TestSyncGroup_Mixed goroutine expected=0 got=%d", decrements)
+				}
+				atomic.AddInt32(&increments, 2)
+			})
+		} else {
+			sg.Go(func() {
+				atomic.AddInt32(&decrements, -1)
+			})
+		}
+	}
+
+	sg.Wait()
+
+	expected := int(iters/10) * 2
+	if atomic.LoadInt32(&increments) != int32(expected) {
+		t.Errorf("TestSyncGroup_Sync expected=%d got=%d", expected, increments)
+	}
+}
+
+func TestSyncGroup_MultiAdd(t *testing.T) {
+	// Add many batches
+	iters := 1000000
+	batches := 100
+	decrements := int32(iters)
+	increments := int32(0)
+
+	sg := &syncgroup.SyncGroup{}
+
+	for j := 0; j < batches; j++ {
+		sg.Add(iters / batches)
+
+		for i := 0; i < iters/batches; i++ {
+			if i%10 == 0 {
+				sg.Go(func() {
+					atomic.AddInt32(&decrements, -1)
+					sg.Sync()
+
+					if atomic.LoadInt32(&decrements) != 0 {
+						t.Errorf("TestSyncGroup_MultiAdd goroutine expected=0 got=%d", decrements)
+					}
+					atomic.AddInt32(&increments, 2)
+				})
+			} else {
+				sg.Go(func() {
+					atomic.AddInt32(&decrements, -1)
+				})
+			}
+		}
+	}
+
+	sg.Wait()
+
+	expected := int(iters/10) * 2
+	if atomic.LoadInt32(&increments) != int32(expected) {
+		t.Errorf("TestSyncGroup_Sync expected=%d got=%d", expected, increments)
+	}
+}
